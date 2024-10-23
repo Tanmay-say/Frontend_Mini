dd1486b26899f18db251d7249e757d6d918857b9
Riddhi1444 <rsdani144@gmail.com>
2024-10-23 11:07:49 +0530

    modified

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index2.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/index2.html")
+def index2():
+    return render_template("index2.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
